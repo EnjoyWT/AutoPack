874ce32b727ff75a73dc2465c06597a6e0d75c32
EnjoyWT <576548368@qq.com>
2016-07-29 11:30:07 +0800

    脚本上传

---
+++ 自动打包上传脚本/dandelion.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#本脚本存放蒲公英apikey
+echo "===========开始打包上传至蒲公英========"
+uKey="e7caf5ab82c8e82"
+apiKey="d8d02ac405fb"
+
+#InspectorMail="测试人员的邮箱"
+#senderName="邮件发送人的名字"
+#copyToAddr="抄送给谁的邮箱地址"
